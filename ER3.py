5681167c574082e1adabb629da4b2db6ada19742
Stependium13 <artemiy.zami@gmail.com>
2024-06-21 22:51:58 -0700

    Every ER has it`s own DB.

--- ER3.py
+++ ER3.py
@@ -1,28 +1,79 @@
+from random import randint, choice
 import socket
 import sqlite3
 from signal import signal, SIGINT
-from random import randint
 import sys
+
+
+def person():
+    return (f'{randint(0, 100)}', f'{randint(0, 100)}', f'{randint(0, 100)}')
+
 
 def signal_handler(sig, frame):
     sys.exit(0)
 
+
+def create_database():
+    conn = sqlite3.connect("ER3.db")
+    cursor = conn.cursor()
+    creation_query = '''
+    CREATE TABLE IF NOT EXISTS Line (Name, Gender, Room)
+    '''
+    cursor.execute(creation_query)
+    person = ('Dave Johnson', 'Male', 'Xray')
+    write_query = '''
+    INSERT INTO Line (Name, Gender, Room)
+    VALUES (?, ?, ?)
+
+    '''
+    cursor.execute(write_query, person)
+    conn.commit()
+    conn.close()
+
+
 def start_server(host='127.0.0.1', port=3333):
+    conn = sqlite3.connect("ER3.db")
+    cursor = conn.cursor()
+    adding_query = '''
+            INSERT INTO Line (Name, Gender, Room)
+            VALUES (?, ?, ?)
+            '''
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((host, port))
         server.listen()
 
         signal(SIGINT, signal_handler)
 
-
         while True:
-            conn, addr = server.accept()
-            data = conn.recv(1024).decode()
+            connect, addr = server.accept()
+            data = connect.recv(1024).decode()
             if data.split(" ")[0] == 'GET':
-                message = f"{randint(1, 5)}"
-                conn.sendall(message.encode())
-                conn.close()
+                roll = randint(1, 20)
+                if roll == 20:
+                    cursor.execute(adding_query, person())
+                    cursor.execute(adding_query, person())
+                if roll > 12:
+                    cursor.execute(adding_query, person())
+                if roll == 1:
+                    cursor.execute("SELECT rowid FROM Line ORDER BY Name LIMIT 2")
+                    rows = cursor.fetchall()
+                    for row in rows:
+                        cursor.execute("DELETE FROM Line WHERE rowid = ?", (row[0],))
+                if roll < 8:
+                    cursor.execute("SELECT rowid FROM Line ORDER BY Name LIMIT 1")
+                    row = cursor.fetchone()
+                    cursor.execute("DELETE FROM Line WHERE rowid = ?", (row[0],))
+                conn.commit()
+                selection_query = '''
+                SELECT COUNT(*) FROM Line
+                '''
+                cursor.execute(selection_query)
+                row = cursor.fetchone()
+                message = f"{row[0]}"
+                connect.sendall(message.encode())
+                connect.close()
 
 
-
+create_database()
 start_server()
--- Server.py
+++ Server.py
@@ -1,21 +1,61 @@
 import socket
+import sqlite3
+
+def create_database(db_name):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    creation_query = '''
+    CREATE TABLE IF NOT EXISTS clinics (name, Address, PostalCode)
+    '''
+    cursor.execute(creation_query)
+    creation_query = '''
+    CREATE TABLE IF NOT EXISTS routes (name, Route)
+    '''
+    cursor.execute(creation_query)
+    adding_query = '''
+    INSERT INTO clinics (Name, Address, PostalCode)
+    VALUES (?, ?, ?)
+    '''
+
+    cursor.execute(adding_query, ('ER1', '123 Main Str', '95610'))
+    cursor.execute(adding_query, ('ER2', '823 High Blvd', '73459'))
+    cursor.execute(adding_query, ('ER3', '992 Sun Ave', '68512'))
+    adding_query = '''
+    INSERT INTO routes (Name, Route)
+    VALUES (?, ?)
+    '''
+
+    cursor.execute(adding_query, ('ER1', '127.0.0.1:1111'))
+    cursor.execute(adding_query, ('ER2', '127.0.0.1:2222'))
+    cursor.execute(adding_query, ('ER3', '127.0.0.1:3333'))
+
+    conn.commit()
 
 def start_client():
 
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-            host = '127.0.0.1'
+            conn = sqlite3.connect('Emergency_Rooms.db')
+            cursor = conn.cursor()
             id = input("Which ER you want to check: ")
+            selection_query = f'''
+            SELECT route FROM routes WHERE name = "ER{id}"
+            '''
+            cursor.execute(selection_query)
+            row = cursor.fetchone()
+            print(row)
+            host, port = row[0].split(':')
             message = f"GET Total FROM {id}"
-            port = int(id) * 1111
-            client.connect((host, port))
+
+            client.connect((host, int(port)))
             client.sendall(message.encode())
             data = client.recv(1024)
             if data:
                 print(f"ER{id} now has total of {data.decode()} customers in line.\n")
             client.close()
 
-
+create_database('Emergency_Rooms.db')
 start_client()
 
 
